43c52d64b3358aa67eb02eb2372ffa062c10e154
chilot <https://github.com/Raigyo>
2020-10-01 12:21:07 +0200

    first commit

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Raigyo/vuex-ideas.git master:gh-pages
+git push -f git@github.com:Raigyo/vuex-shop.git master:gh-pages
 
 cd -
