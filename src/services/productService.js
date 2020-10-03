f60d58d058bb5043c542b208f83a408ae7fed38c
chilot <https://github.com/Raigyo>
2020-10-03 19:27:13 +0200

    branch local-version

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,8 +2,8 @@
 
 import axios from "axios";
 
-let baseUrl = "https://my-json-server.typicode.com/raigyo/vue-shop-json-server";
-//let baseUrl = "http://localhost:3000";
+// let baseUrl = "https://my-json-server.typicode.com/raigyo/vue-shop-json-server";
+let baseUrl = "http://localhost:3000";
 
 export default {
   getProducts() {
