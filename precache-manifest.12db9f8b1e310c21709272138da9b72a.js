fc149195384b2787842af5d3dc228d6606c41a54
chilot <https://github.com/Raigyo>
2020-10-03 19:30:24 +0200

    deploy

---
+++ precache-manifest.12db9f8b1e310c21709272138da9b72a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c7825ed00a5e16cb90",
+    "url": "/vuex-shop/css/app.d454043f.css"
+  },
+  {
+    "revision": "fa5b2a55a8242c658a0b",
+    "url": "/vuex-shop/css/chunk-489ec226.05f1378b.css"
+  },
+  {
+    "revision": "ebd72e981ee888aabce6",
+    "url": "/vuex-shop/css/chunk-vendors.6612108b.css"
+  },
+  {
+    "revision": "a648a38b2425c7aa64b24f67799b0fc2",
+    "url": "/vuex-shop/index.html"
+  },
+  {
+    "revision": "d1649446a03a85dfed92",
+    "url": "/vuex-shop/js/about.6d95b6de.js"
+  },
+  {
+    "revision": "75c7825ed00a5e16cb90",
+    "url": "/vuex-shop/js/app.24c93047.js"
+  },
+  {
+    "revision": "d99691cbf28a6403fcff",
+    "url": "/vuex-shop/js/chunk-2d21df6b.c9ff29b6.js"
+  },
+  {
+    "revision": "64c2e0793a6dd6a8bc94",
+    "url": "/vuex-shop/js/chunk-2d230284.d819b360.js"
+  },
+  {
+    "revision": "fa5b2a55a8242c658a0b",
+    "url": "/vuex-shop/js/chunk-489ec226.c701fb20.js"
+  },
+  {
+    "revision": "ebd72e981ee888aabce6",
+    "url": "/vuex-shop/js/chunk-vendors.896af7b5.js"
+  },
+  {
+    "revision": "ceb68a5243b6027222ae063fab0584ab",
+    "url": "/vuex-shop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuex-shop/robots.txt"
+  }
+]);
